23e8ff8542bd679c54191e627a5bad9686b2a8ef
Ruan Cunha <32649330+ruan-cunha@users.noreply.github.com>
2025-08-11 18:24:36 -0300

    Update news.js

--- news.js
+++ news.js
@@ -4,14 +4,14 @@ const newsData = [
         category: "GLOBAL OPERATIONS",
         date: "2018-11-16",
         author: "ARISA Global Command",
-        body: "ARISA has officially confirmed a successful joint operation involving assets from The Paramount and The Vanguards. The mission, classified until this morning, involved the stabilization of a rogue geo-political event. Assets 'Downdraft' and 'Kinesis' were instrumental in evacuating civilians and providing tactical mobility in a hostile environment."
+        body: "ARISA has officially confirmed a successful joint operation involving operative from The Paramount and The Vanguards. The mission, classified until this morning, involved the stabilization of a rogue geo-political event. Operatives 'Downdraft' and 'Kinesis' were instrumental in evacuating civilians and providing tactical mobility in a hostile environment."
     },
     {
         headline: "The Paramount's 'Armory' Neutralizes High-Tech Threat in Neo-Tokyo",
         category: "TECHNOLOGY DIVISION",
         date: "2018-11-12",
         author: "ARISA Press Office",
-        body: "A significant threat involving illegal sentient robotics was neutralized in Neo-Tokyo by Paramount asset 'Armory'. Her ability to manifest adaptive weaponry was critical in disabling the hostile network without triggering its city-wide failsafe. The operation highlights the growing need for specialized awakened responses to technological threats."
+        body: "A significant threat involving illegal sentient robotics was neutralized in Neo-Tokyo by Paramount Operative 'Armory'. Her ability to manifest adaptive weaponry was critical in disabling the hostile network without triggering its city-wide failsafe. The operation highlights the growing need for specialized awakened responses to technological threats."
     },
     {
         headline: "Director of ARISA to Address UN Security Council on 'The Third Awakening'",
@@ -25,6 +25,7 @@ const newsData = [
         category: "DOMESTIC AFFAIRS",
         date: "2018-10-30",
         author: "ARISA Public Affairs",
-        body: "The Vanguards initiative has been publicly commended for its rapid and effective response to a mag-lev transport malfunction on the west coast. Assets 'Flashpoint' and 'Chisel' successfully halted the runaway vehicle, preventing a major catastrophe. The incident underscores the value and effectiveness of the youth initiative under extreme pressure."
+        body: "The Vanguards initiative has been publicly commended for its rapid and effective response to a mag-lev transport malfunction on the west coast. Operatives 'Flashpoint' and 'Chisel' successfully halted the runaway vehicle, preventing a major catastrophe. The incident underscores the value and effectiveness of the youth initiative under extreme pressure."
     }
 ];
+
