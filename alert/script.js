02f58eb5c6be3d8cfcc6bbfc4475a68c918de46c
Ruan Cunha <32649330+ruan-cunha@users.noreply.github.com>
2025-08-18 16:37:10 -0300

    Update script.js

--- alert/script.js
+++ alert/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- MUDANÇA 1: VOLUME E AUTOPLAY ---
     // Volumes foram reduzidos significativamente.
-    alertSound.volume = 0.010;
+    alertSound.volume = 0.01;
     ambientSound.volume = 0.05;
 
     // Tentativa de tocar o áudio assim que a página carrega.
@@ -70,5 +70,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     displayNextMessage();
 });
+
 
 
