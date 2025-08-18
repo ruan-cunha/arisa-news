b2268e65be1b7b3276b7ccc7b530c3d0187a043d
Ruan Cunha <32649330+ruan-cunha@users.noreply.github.com>
2025-08-18 16:55:53 -0300

    Update script.js

--- alert/script.js
+++ alert/script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         "<p><strong>SUBJECT PROFILE:</strong> Single hostile entity, ~25 meters in height. Skeletal form with charred, ceramic-like carapace. Fissures in carapace emit high-temperature golden glow.</p>",
         "<p><strong>DIRECTIVE 1 [CIVILIAN POPULATIONS]: THIS IS NOT A DRILL.</strong> All civilians in the greater Chennai area must seek immediate, reinforced, subterranean shelter. Evacuate all coastal and open areas.</p>",
         "<p><strong>DIRECTIVE 2 [NON-COMBAT ASSETS]: STAND DOWN.</strong> All local and independent Awakened without direct combat-rating are ordered to cease operations and assist with civilian evacuation efforts ONLY.</p>",
-        "<p class='omega-protocol'>** // OMEGA PROTOCOL INITIATED // **</p>",
+        "<p class='omega-protocol'>** // SOVEREIGN PROTOCOL INITIATED // **</p>",
         "<p><strong>By order of the Axis Mundi Global Security Council, a general amnesty is in effect. All combat-capable assets, regardless of affiliation or prior designation, are now requested for emergency reinforcement to combat an Existential-Level Threat.</strong></p>",
         "<p><strong>TRANSPORT:</strong> Transport will be dispatched globally for all combat-capable assets responding to the call.</p>",
         "<p><strong>TRANSMISSION ACTIVE... AWAITING RESPONSE...</strong></p>"
@@ -64,6 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     displayNextMessage();
 });
+
 
 
 
